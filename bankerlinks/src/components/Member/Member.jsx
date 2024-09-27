f029c4b003d1b22b6b1df6e5a1ecd1c7f4ab3312
Vitaly <82165471+VKrivkov@users.noreply.github.com>
2024-09-27 12:21:48 +0200

    Team Adjusted

--- bankerlinks/src/components/Member/Member.jsx
+++ bankerlinks/src/components/Member/Member.jsx
@@ -8,8 +8,7 @@ const Member = ({name, position, p, color}) => {
     <div className='member-card'>
       <div className='wrapper'>
         <div className='upper-text'>
-          <h5>{name},</h5>
-          <h6>{position}</h6>
+          <h5 className='member-name'>{name}, <span>{position}</span></h5>
         </div>
         <p>{p}</p>
       </div>
